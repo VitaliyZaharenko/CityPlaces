3166232314bd9cbcd134dbe3fcb91f2b040cd205
VitaliZahar <vitalizahar@gmail.com>
2018-10-24 01:32:46 -0700

    Implement map type switching

---
+++ CityPlaces/Constants/Consts.swift
@@ -0,0 +1,24 @@
+//
+//  Consts.swift
+//  CityPlaces
+//
+//  Created by vitali on 10/23/18.
+//  Copyright © 2018 zaharenkov. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    
+    struct StandartBarItem {
+        static let tag = 0
+    }
+    
+    struct SatelliteBarItem {
+        static let tag = 1
+    }
+    
+    struct HybridBarItem {
+        static let tag = 2
+    }
+}
--- CityPlaces/Controllers/MapViewController.swift
+++ CityPlaces/Controllers/MapViewController.swift
@@ -9,22 +9,71 @@
 import UIKit
 import MapKit
 
+
+fileprivate enum MapType: Int {
+    
+    case standart
+    case satellite
+    case hybrid
+}
+
 class MapViewController: UIViewController {
     
     //MARK: - Views
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tabBar: UITabBar!
     
-
+    //MARK: - Properties
+    
+    private var mapType: MapType = .standart {
+        didSet {
+            setMapType()
+        }
+    }
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureTabBar()
+        configureMapView()
+    }
+    
+    
+}
+
+//MARK: Private Help Methods
+
+extension MapViewController {
+    
+    private func configureTabBar(){
+        tabBar.delegate = self
+        tabBar.selectedItem = tabBar.items!.first
+    }
+    
+    private func configureMapView(){
+        
+        mapView.delegate = self
+        
         let point = PointManager.shared.all().first!
         focusMap(on: point, withDistance: 100000)
-        
+        addAnnotation(on: point, title: "GOMEL MARKER", subtitle: nil)
     }
+    
+    private func setMapType(){
+        print(mapType)
+        switch mapType {
+        case .standart:
+            mapView.mapType = .standard
+        case .satellite:
+            mapView.mapType = .satellite
+        case .hybrid:
+            mapView.mapType = .hybrid
+        }
+    }
+    
     
     private func focusMap(on point: Point, withDistance: Double) {
         let focusLocation = CLLocation(latitude: point.lat, longitude: point.lon)
@@ -33,8 +82,40 @@ class MapViewController: UIViewController {
     }
     
     
-//    private func addArtwork(on point: Point, title: String, locationName: String, discipline: String){
-//        let artwork = Artwo
-//    }
+    private func addAnnotation(on point: Point, title: String?, subtitle: String?){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocation(latitude: point.lat, longitude: point.lon).coordinate
+        annotation.title = title
+        annotation.subtitle = subtitle
+        mapView.addAnnotation(annotation)
+    }
+}
+
+
+//MARK: - UITabBarDelegate
+
+extension MapViewController: UITabBarDelegate {
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case Consts.StandartBarItem.tag:
+            mapType = .standart
+        case Consts.SatelliteBarItem.tag:
+            mapType = .satellite
+        case Consts.HybridBarItem.tag:
+            mapType = .hybrid
+        default:
+            fatalError("Unknown TabBarItem tag = \(item.tag)")
+        }
+    }
+}
+
+//MARK: - MKMapViewDelegate
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print(view.annotation?.title)
+    }
 }
 
